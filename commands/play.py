ce114a8be48002bbb042ff15af762190c5e14dbb
ghost <lan3r@proton.me>
2025-06-27 05:06:50 -0300

    2.3.11: adiciona o descrivão e o autocompletador

--- commands/play.py
+++ commands/play.py
@@ -2,10 +2,14 @@ from database.Database import Database
 import asyncio
 from classes.YTDLSource import YTDLSource
 from classes.Utils import Utils
+from discord import app_commands
+import discord
 
 # DESCRIPTION: Toca um áudio da lista de áudios
 def setup(bot):
     @bot.tree.command(name="play", description="Toca um áudio")
+    @app_commands.describe(name="Nome do áudio")
+    @app_commands.autocomplete(name=autocomplete_audios)
     async def play_audio(interaction, name: str):
         await interaction.response.defer()
 
@@ -35,3 +39,14 @@ def setup(bot):
             if voice_client.is_connected():
                 await voice_client.disconnect()
             await interaction.followup.send(f"❌ Erro ao reproduzir áudio: {str(e)}")
+
+async def autocomplete_audios(interaction: discord.Interaction, current: str):
+    try:
+        all_audios = Database().get_database().keys()
+        suggestions = [a for a in all_audios if current.lower() in a.lower()]
+        return [
+            app_commands.Choice(name=nome, value=nome)
+            for nome in suggestions[:25]
+        ]
+    except Exception as e:
+        return [e]
