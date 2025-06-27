0084c9fe1c5d8a863b155e9d2d4bc8d3533b4dee
ghost <lan3r@proton.me>
2025-06-27 19:00:45 -0300

    2.6.17: total revamp of the auto_complete files and system

--- commands/play.py
+++ commands/play.py
@@ -2,14 +2,14 @@ from database.Database import Database
 import asyncio
 from classes.YTDLSource import YTDLSource
 from classes.Utils import Utils
+from database.on_search import on_search
 from discord import app_commands
-import discord
 
 # DESCRIPTION: Toca um áudio da lista de áudios
 def setup(bot):
     @bot.tree.command(name="play", description="Toca um áudio")
     @app_commands.describe(name="Nome do áudio")
-    @app_commands.autocomplete(name=autocomplete_audios)
+    @app_commands.autocomplete(name=on_search)
     async def play_audio(interaction, name: str):
         await interaction.response.defer()
 
@@ -39,26 +39,3 @@ def setup(bot):
             if voice_client.is_connected():
                 await voice_client.disconnect()
             await interaction.followup.send(f"❌ Erro ao reproduzir áudio: {str(e)}")
-
-
-async def autocomplete_audios(interaction: discord.Interaction, current: str):
-    try:
-        all_audios = list(Database().get_database().keys())  #
-        suggestions = []
-
-        if 0 < len(current) < 3:
-            short_audio_names = [name for name in all_audios if len(name) < 6]
-
-            suggestions = [s for s in short_audio_names if current.lower() in s.lower()]
-
-        elif len(current) >= 3:
-            suggestions = [a for a in all_audios if current.lower() in a.lower()]
-
-        return [
-            app_commands.Choice(name=nome, value=nome)
-            for nome in suggestions[:25]
-        ]
-
-    except Exception as e:
-        print(f"Error during autocomplete: {e}")
-        return []
